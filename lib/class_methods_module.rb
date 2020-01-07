869bc7885e1f24db143e309e7c69e597ca5a72df
Zach Cullen <zacullen@gmail.com>
2020-01-07 21:50:35 +0000

    Automatically backed up by Learn

--- lib/class_methods_module.rb
+++ lib/class_methods_module.rb
@@ -3,4 +3,13 @@ module MetaDancing
   def metadata
     "This class produces objects that love to dance."
   end
+  
+end
+
+class Dancer
+  extend MetaDancing
+end
+
+class Kid
+  extend MetaDancing
 end
