72acc34dc38fa6136105cacc5488e739be82d238
Zach Cullen <zacullen@gmail.com>
2020-01-07 22:01:59 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -4,4 +4,10 @@ require_relative './fancy_dance.rb'
 class Kid
   extend FancyDance::ClassMethods 
   include FancyDance::InstanceMethods
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+  end
 end
